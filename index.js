aadcaadc405d979222bc766777e80f81289eaa67
AlvaroJorge <alv.jor96@gmail.com>
2022-01-26 21:56:49 -0300

    JS final. Agregado span

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-var textoInput;
-var textoEncript;
-var textoDecript;
-var textoCopiado;
-var validar = false;
+let textoInput = document.getElementById('txt_input');
+let textoCopiado = document.getElementById('txt_cop');
+let spanWarning = document.querySelector('.span_txt');
+let spanCopiado = document.querySelector('.span_copiado');
+let textoEncript;
+let textoDecript;
+let verificador = false;
 
 let btn_encript = document.querySelector('.btn_encrip');
 let btn_decript = document.querySelector('.btn_descrip');
 let btn_copiar = document.querySelector('.btn_cop');
 
-function seleccionartexto(){
-    var textoInput = document.getElementById('txt_msj').value;
-    if (/[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/.test(textoInput)||
-        (/[0-9]/.test(textoInput))||
-        (/[A-Z]/.test(textoInput))){
-        alert("Escribe solo con letras minúsculas");    
-        validar=false;
-    }
-    else if ((textoInput=="")){   
-        validar=false;
-    }
-    else{
-        validar=true;
+//EVENTOS BOTONES
+
+btn_encript.addEventListener('click', encriptador);
+
+btn_decript.addEventListener('click', decriptador);
+
+btn_copiar.addEventListener('click', copiarTexto);
+
+//FUNCION VALIDAR CARACTERES
+
+function validarInput(){
+    caracteres = document.getElementById('txt_input').value;
+    if (/[A-Z]/.test(caracteres) || (/[0-9]/.test(caracteres)) || (/[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/.test(caracteres))) {
+        verificador = false;
+        spanWarning.textContent = 'Solo letras minúsculas sin signos!';
+    } else if (caracteres == '') {
+        verificador = false; 
+        spanWarning.textContent = 'Escriba un mensaje!';
+    } else {
+        verificador = true;
     }
 }
 
-function encriptarTexto(){
-    var textoInput = document.getElementById('txt_msj').value;
-    seleccionartexto();
-    if (validar==true) {
-        var textoEncript = textoInput.replace(/a/g, 'ai');
-        var textoEncript = textoEncript.replace(/e/g, 'enter');
-        var textoEncript = textoEncript.replace(/i/g, 'imes');
-        var textoEncript = textoEncript.replace(/o/g, 'ober');
-        var textoEncript = textoEncript.replace(/u/g, 'ufat');
+//FUNCION ENCRIPTAR MENSAJE
 
-        document.getElementById('txt_cop').value = textoEncript;
-        document.getElementById('txt_msj').value = '';
+function encriptador(){
+    validarInput();
+    caracteres = textoInput.value;
+    if (verificador == true) {
+        textoEncript = caracteres.replaceAll('e', 'enter')
+                                 .replaceAll('i', 'imes')
+                                 .replaceAll('a', 'ai')
+                                 .replaceAll('o', 'ober')
+                                 .replaceAll('u', 'ufat');
+        textoCopiado.value = textoEncript;
+        textoInput.value = '';
+        spanWarning.textContent = 'Mensaje Encriptado!';
     }
 }
 
-function desencriptarTexto() {
-    var textoInput = document.getElementById('txt_msj').value;
-    seleccionartexto();
-    if (validar==true){
-        var textoDecript = textoInput.replace(/ai/g, 'a');
-        var textoDecript = textoDecript.replace(/enter/g, 'e');
-        var textoDecript = textoDecript.replace(/imes/g, 'i');
-        var textoDecript = textoDecript.replace(/ober/g, 'o');
-        var textoDecript = textoDecript.replace(/ufat/g, 'u');
-    
-        document.getElementById("txt_cop").value = textoDecript;
-        document.getElementById("txt_msj").value = '';
+//FUNCION DESENCRIPTAR MENSAJE
+
+function decriptador(){
+    validarInput();
+    caracteres = textoInput.value;
+    if (verificador == true) {
+        textoDecript = caracteres.replaceAll('enter', 'e')
+                                 .replaceAll('imes', 'i')
+                                 .replaceAll('ai', 'a')
+                                 .replaceAll('ober', 'o')
+                                 .replaceAll('ufat', 'u');
+        textoCopiado.value = textoDecript;
+        textoInput.value = '';
+        spanWarning.textContent = 'Mensaje Desencriptado!';
     }
 }
 
-function copiar() {
-    var textoCopiado = document.getElementById("txt_cop");
+//FUNCION COPIAR
+
+function copiarTexto(){
+    textoCopiado = document.getElementById('txt_cop');
     textoCopiado.select();
     navigator.clipboard.writeText(textoCopiado.value);
-    
-}
-  
-function evitarRecarga(event){  
-    event.preventDefault();
-    return false;
+    spanCopiado.textContent = 'Mensaje Copiado!';
 }
 
-btn_encript.addEventListener('click', encriptarTexto);
-btn_decript.addEventListener('click', desencriptarTexto);
-btn_copiar.addEventListener('click', copiar);
